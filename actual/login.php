46cca4a4f4d97d04f9d147d78213dd8cee6fff5a
men-ka <92216401+men-ka@users.noreply.github.com>
2023-08-23 13:36:53 +0545

    Update login.php
    different page after login for user and admin

--- actual/login.php
+++ actual/login.php
@@ -8,17 +8,21 @@
       $password= $_POST["lpassword"];
 
       $sql ="SELECT * FROM users WHERE username='$username' AND password='$password'";
-      $result=mysqli_query($conn,$sql);
-      $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-      $count = mysqli_num_rows($result);
-
-      if($count==1){
-        header("Location: trylogin.html");
-        exit();
-      }
-      else{
-        echo '<script>alert("Invalid Email or Password")</script>';
-      }
+      $result = $conn->query($sql);
+        if($result->num_rows>0){
+            while ($row = $result->fetch_assoc()){
+                $accountType = $row['accountType'];
+            } 
+          if($accountType=='admin'){
+            header("Location: desktopA.html");
+          }else{
+            header("Location: desktop1.html");
+          }
+          exit();
+        }
+        else{
+          echo '<script>alert("Invalid Email or Password")</script>';
+        }  
     }
   
   }
